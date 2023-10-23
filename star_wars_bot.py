6120afb3fc351ed3415220a683802c32aee93fd3
Matt <mattsantha@gmail.com>
2023-10-23 08:43:20 -0700

    Made quiz formate

--- star_wars_bot.py
+++ star_wars_bot.py
@@ -4,9 +4,9 @@
 
 import time
 
-# Create a bot that with a series of yes or no questions that will decied weather your on the light side or dark side
+# Create a bot that with a series of yes or no questions that will decide weather your on the light side or dark side
 
-print("Hello I am a Star wars chatbot that will decied if you belong on the light or dark side.")
+print("Hello I am a Star wars chatbot that will decide if you belong on the light or dark side.")
 time.sleep(1)
 print("Please answer these yes or no questions honestly.")
 time.sleep(1)
@@ -18,8 +18,8 @@ cape_question = input("Do you like wearing capes?")
 
 red_question = input("Do you like the coulor red?")
 
-# Decied if the user is on the dark or light side based on the questions
+# Decide if the user is on the dark or light side based on the questions
 if cape_question.lower().strip(",.?!") or red_question.lower().strip(",.?!") == "yes":
     print("You belong to the dark side of the force.")
 else: 
-    print("I sence the light side within you.")
+    print("I sense the light side within you.")
