c142e62c0dceddaecba897209b0d7f4a28c9cf5d
Matt <mattsantha@gmail.com>
2023-10-30 09:20:55 -0700

    added a rounding

--- Notes/fave_bubble_tea.py
+++ Notes/fave_bubble_tea.py
@@ -15,7 +15,7 @@ coco_likes = 0         # Intitialize the varable to 0
 suntea_likes = 0
 chatime_likes = 0
 bubqueen_likes = 0
-
+other_likes = 0
 
 for _ in range(NUM_RESPONDENTS): 
     # Ask the user what thier favorite place is
@@ -35,13 +35,16 @@ for _ in range(NUM_RESPONDENTS):
         bubqueen_likes += 1
     elif fave_place == "chatime":
         chatime_likes += 1
+    else: 
+        other_likes +=1
 
 # Repeat the code above 5 times
 
 
 # print out summary
 # Give the raw score and the score percentage
-print(f"CoCo likes: {coco_likes} / {coco_likes / NUM_RESPONDENTS * 100}%")
-print(f"Suntea likes: {suntea_likes} / {suntea_likes / NUM_RESPONDENTS * 100}%")
-print(f"Bubble Queen likes: {bubqueen_likes} / {bubqueen_likes / NUM_RESPONDENTS * 100}%")
-print(f"Chatime likes: {chatime_likes} / {chatime_likes / NUM_RESPONDENTS * 100}%")
+print(f"CoCo likes: {coco_likes} / {round (coco_likes / NUM_RESPONDENTS * 100)}%")
+print(f"Suntea likes: {suntea_likes} / {round (suntea_likes / NUM_RESPONDENTS * 100)}%")
+print(f"Bubble Queen likes: {bubqueen_likes} / {round (bubqueen_likes / NUM_RESPONDENTS * 100)}%")
+print(f"Chatime likes: {chatime_likes} / {round (chatime_likes / NUM_RESPONDENTS * 100)}%")
+print(f"Other likes : {other_likes} / {round (other_likes / NUM_RESPONDENTS * 100)}%")
