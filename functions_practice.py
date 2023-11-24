5110504581b5afb14809c07027fe4e4e852b7dc2
Matt <mattsantha@gmail.com>
2023-11-24 15:03:09 -0800

    cookie code

---
+++ functions_practice.py
@@ -0,0 +1,43 @@
+# Function Practice
+# Author: Matt
+# Date: November 24 2023
+
+def area_of_a_square(sidelength: float) -> float:
+    """Return the area of a square.
+    Results are in units squared.
+
+    Params:
+
+    sidelength - length of one side of the square
+    """
+
+    area = sidelength**2
+
+    return area
+
+
+
+	
+def print_area_of_a_square(sidelength: float):
+	"""Calculate and print the area of a sqaure.
+	Results are in units squared
+	
+	Params:
+	
+	sidelength - length of one side of the square
+	"""
+	area = sidelength ** 2 
+	
+	print(f"The area of a square with side length {sidelength} is: {area} square units"
+       )
+
+print(print_area_of_a_square(12.2))
+# print_area_of_a_square(12)
+
+# Given two squares of two sidelengths
+#    12.2 and 144
+# Add the area of both squares
+
+# area_of_squares = area_of_a_square(12.2) + area_of_a_square(144)
+# print(area_of_squares)
+
