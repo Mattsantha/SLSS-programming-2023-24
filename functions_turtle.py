2b5366e4974280e7ff725a0c974c53b585ed0a98
Matt <mattsantha@gmail.com>
2023-12-05 09:46:40 -0800

    more burt :D + some notes

--- functions_turtle.py
+++ functions_turtle.py
@@ -63,13 +63,47 @@ def draw_tree(level: int, height: int) -> None:
         burt.color(original_colour[0]) # revert burt's color
 
 # Set-up Burt to draw trees
+
+def draw_fancy_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree with initial given height in pixels
+    
+    Parmas:
+    
+    level - number representing the levels of branches
+    height - height of the main trunk in pixels
+    """
+
+    if level > 0:
+        # 1. Move turtle forward height pixels
+        burt.forward(height)
+
+        # 2. Turn turtle left
+        burt.left(36)
+        #    a. Draw a smaller tree (current level - 1)
+        draw_fancy_tree(level - 1, height * .75)
+
+        burt.right(40)
+        #    a. Draw a smaller tree (current level - 1)
+        draw_fancy_tree(level - 1, height * .75)
+
+        # 3. Turn turtle right
+        burt.right(45)
+        #    a. Draw a smaller tree (current level - 1)
+        draw_fancy_tree(level - 1, height * .9)
+
+        # 4. Return to beginning
+        burt.left(49)
+        burt.back(height)
+    else:
+        original_colour = burt.color()
+        burt.color("green")
+        burt.stamp()
+        burt.color(original_colour[0]) # revert burt's color
+
 burt.color("brown")
 burt.setheading(90)     # Points Burt north
 burt.width(4)           # Trunck and branches thinker
-burt.speed(5)
+burt.speed(0)
 
-draw_tree(1, 175)
-
-
-
+draw_fancy_tree(3, 150)
 turtle.done()
