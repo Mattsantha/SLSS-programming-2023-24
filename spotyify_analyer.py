e94885f149564e8a311d9d6232e0ca0322a5a53e
Matt <mattsantha@gmail.com>
2024-01-16 08:52:38 -0800

    ©

---
+++ spotyify_analyer.py
@@ -0,0 +1,15 @@
+# Spotify Analyzer
+# Author: Matt
+# 16 January 2024
+
+
+# Open the file
+
+# ---- Search for all Drake songs
+# ---- USe linear search 
+
+# Make a list to stroe all Drake songs
+
+# For every line in the file 
+    # If the artist for thissomng is Drake
+        # Store the song in the list
