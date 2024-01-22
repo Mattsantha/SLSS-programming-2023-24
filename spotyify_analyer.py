d9603f0bfa00e5963c88e27aa2e50647c8043df9
Matt <mattsantha@gmail.com>
2024-01-22 13:46:13 -0800

    songs

--- spotyify_analyer.py
+++ spotyify_analyer.py
@@ -49,6 +49,7 @@ def find_all_songs(artist: str) -> list:
                     songs.append((
                         line['artist'],
                         line['song_title'],
+                        line['valence'],
                         line['danceability']
                     ))
                 line_num += 1
@@ -60,6 +61,44 @@ ed_sheran_songs = find_all_songs("ed shearan")
 kendrick_songs = find_all_songs("kendrick")
 
 #print out drake songs that have a danceability of 0.5 or higher
-for song in kendrick_songs:
-    if float(song[-1]) >= 0.5:
-        print(song)
+#for song in kendrick_songs:
+    #if float(song[-1]) >= 0.5:
+       # print(song)
+
+
+# Right now our songs have three pieces of info:
+#    - artist
+#    - song title
+#    - danceability
+# Can you add one more piece of info for every song:
+#    - valence
+# Right now: ('artist', 'song_title', 'danceability')
+# Change: ('artist', 'song_title', 'valence', 'danceability')
+
+# --- Sort using SLECTION sort from smallest to biggest
+#   - Dancebility
+
+# Startibg at the begging of the list until the end
+for i, song in enumerate(drake_songs):
+    # Set the current item to the smnallest danceability
+    smallest_dancebility = song[-1]
+    smallest_idx = i
+
+    # For the rest of the list
+    for j in range(i + 1, len(drake_songs)):
+        # If the current item is smaller than smallest
+        if drake_songs[j][-1] > smallest_dancebility:
+            # Set the smallest danceability with the current
+            smallest_dancebility = drake_songs[j][-1]
+            smallest_idx = j
+        
+    # Swap the smalllest danceability with the current item
+    drake_songs[i], drake_songs[smallest_idx] = (
+        drake_songs[smallest_idx],
+        drake_songs[i]
+    )
+
+for song in drake_songs:
+     print(song)
+
+# can you modify the code so that it sorts from biggest to smallest
